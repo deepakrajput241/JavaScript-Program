fe335c8e17f9237406e14b4f68877bb25e0e249a
Deepakrajput241 <deepakrajput241@gmail.com>
2023-10-24 13:18:10 +0530

    datatype

---
+++ js-basic/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144534;
+let accountEmail = 'Deepak@gmail.com';
+var accountPassword = "12548";
+accountCity = "Kanpur";
+//Not allowed
+//number, boolean, bigint, null, undefined, symbol- primitive datatype
+//object
+// accountId = 2;
+// console.log(accountId);
+accountEmail = "gc@yd.com"
+console.table([accountEmail, accountId, accountPassword, accountCity]);
+console.log(typeof null);
+console.log(typeof undefined);
